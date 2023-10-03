c207c337915ebe1e78d17bf41b71b40a0e540180
Víctor Souza Moreira Quirino <victorsouzaq@gmail.com>
2023-10-03 11:02:08 -0300

    Pratica aula 21 - Exercício senha: método gerar senha aleatória

--- PraticaAula21/exercicio_valida_senha/Senha.cpp
+++ PraticaAula21/exercicio_valida_senha/Senha.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Senha{
     public:
         static bool validar_senha(string);
+        static string gerar_senha();
 };
 
 bool Senha::validar_senha(string senha){
@@ -16,4 +17,14 @@ bool Senha::validar_senha(string senha){
     }
 
     return (minusculas && maiusculas && numeros);
+}
+
+string Senha::gerar_senha(){
+    string senha = "";
+    char caracteres[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    
+    for(int i = 0; i < 8; i++) senha.push_back(caracteres[rand() % 62]);
+    return senha;
 }
--- PraticaAula21/exercicio_valida_senha/main.cpp
+++ PraticaAula21/exercicio_valida_senha/main.cpp
@@ -12,5 +12,7 @@ int main(){
     if(!Senha::validar_senha(senha)) cout<<"Senha invalida!"<<endl;
     else cout<<"Senha valida!"<<endl;
 
+    cout<<"Senha gerada automaticamente: "<<Senha::gerar_senha()<<endl;
+
     return 0;
 }
