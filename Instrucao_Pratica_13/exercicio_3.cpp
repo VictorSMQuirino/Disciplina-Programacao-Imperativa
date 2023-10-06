e844295daea342754ae526540927b8ccdb87f1a5
Víctor Quirino <victorsouzaq@gmail.com>
2023-10-06 08:56:41 -0300

    Inicio de implementação do exercício 3 da instrução prática 13

---
+++ Instrucao_Pratica_13/exercicio_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Produto{
+    string codigo;
+    string nome;
+    double preco;
+};
+
+void inserir_produto(vector<Produto> &);
+
+int main(){
+    return 0;
+}
+
+void inserir_produto(vector<Produto> &produtos){
+    
+}
